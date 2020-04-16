e9ba2dc6ae33adef911b865cd40a93a7ae69f271
Fethi Isfarca <fisfarca@googlemail.com>
2020-04-16 20:45:59 +0200

    First version
    Worked version.

---
+++ multilanguage/Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+﻿using Polyglot;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(LocalizationImporter.DownloadCustomSheet());
+    }
+}
